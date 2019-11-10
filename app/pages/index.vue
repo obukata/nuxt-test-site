286ca500b44a4a9c8a20cb73caa54d35afbc28b2
生方健成 <squib02@gmail.com>
2019-11-10 17:42:08 +0900

    Add file index.vue 設置のみ

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,77 +1,29 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-test-site
-      </h1>
-      <h2 class="subtitle">
-        Easy site creation using nuxt
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<div>
+		<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+			<div class="flex items-center flex-shrink-0 text-white mr-6">
+				<span class="font-semibold text-xl tracking-tight">Nuxt Test Site</span>
+			</div>
+			<div class="block lg:hidden">
+				<button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+					<title>Menu</title>
+				</button>
+			</div>
+		</nav>
+		<div class="container mx-auto px-4">
+			<h1>Nuxt Test Site</h1>
+		</div>
+	</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
 </script>
 
 <style>
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+	@apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
