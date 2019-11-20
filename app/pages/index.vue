72762290f6d73e545cfcc82446700b12d4d122f3
生方健成 <squib02@gmail.com>
2019-11-20 23:51:05 +0900

    Update file create post list

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,6 +8,112 @@
 					</h1>
 				</div>
 			</div>
+
+			<article class="container mx-auto">
+				<section class="mt-10 m-auto p-4">
+					<h2 class="contents-title font-bold text-4xl tracking-widest p-4">News Posts</h2>
+					<div class="contents-body px-4">
+						<ul class="contents-list block md:flex flex-wrap md:-mx-2">
+							<li class="contents-item w-full xl:w-1/4 lg:w-1/3 md:w-1/2 md:p-2 mb-4 md:mb-0">
+								<nuxt-link class="block bg-white rounded-lg overflow-hidden shadow" to="####">
+									<img class="h-56 w-full object-cover object-center" src="/assets/images/index/img-mainvisual.jpg" alt="Sunset in the mountains">
+									<div class="px-6 py-4">
+										<div class="font-bold text-xl mb-2">Posts Title</div>
+										<p class="text-gray-700 text-base">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+										</p>
+									</div>
+									<div class="px-6 py-4">
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Design</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Animation</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Fature</span>
+									</div>
+								</nuxt-link>
+							</li>
+							<li class="contents-item w-full xl:w-1/4 lg:w-1/3 md:w-1/2 md:p-2 mb-4 md:mb-0">
+								<nuxt-link class="block bg-white rounded-lg overflow-hidden shadow" to="####">
+									<img class="h-56 w-full object-cover object-center" src="/assets/images/index/img-mainvisual.jpg" alt="Sunset in the mountains">
+									<div class="px-6 py-4">
+										<div class="font-bold text-xl mb-2">Posts Title</div>
+										<p class="text-gray-700 text-base">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+										</p>
+									</div>
+									<div class="px-6 py-4">
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Design</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Animation</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Fature</span>
+									</div>
+								</nuxt-link>
+							</li>
+							<li class="contents-item w-full xl:w-1/4 lg:w-1/3 md:w-1/2 md:p-2 mb-4 md:mb-0">
+								<nuxt-link class="block bg-white rounded-lg overflow-hidden shadow" to="####">
+									<img class="h-56 w-full object-cover object-center" src="/assets/images/index/img-mainvisual.jpg" alt="Sunset in the mountains">
+									<div class="px-6 py-4">
+										<div class="font-bold text-xl mb-2">Posts Title</div>
+										<p class="text-gray-700 text-base">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+										</p>
+									</div>
+									<div class="px-6 py-4">
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Design</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Animation</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Fature</span>
+									</div>
+								</nuxt-link>
+							</li>
+							<li class="contents-item w-full xl:w-1/4 lg:w-1/3 md:w-1/2 md:p-2 mb-4 md:mb-0">
+								<nuxt-link class="block bg-white rounded-lg overflow-hidden shadow" to="####">
+									<img class="h-56 w-full object-cover object-center" src="/assets/images/index/img-mainvisual.jpg" alt="Sunset in the mountains">
+									<div class="px-6 py-4">
+										<div class="font-bold text-xl mb-2">Posts Title</div>
+										<p class="text-gray-700 text-base">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+										</p>
+									</div>
+									<div class="px-6 py-4">
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Design</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Animation</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Fature</span>
+									</div>
+								</nuxt-link>
+							</li>
+							<li class="contents-item w-full xl:w-1/4 lg:w-1/3 md:w-1/2 md:p-2 mb-4 md:mb-0">
+								<nuxt-link class="block bg-white rounded-lg overflow-hidden shadow" to="####">
+									<img class="h-56 w-full object-cover object-center" src="/assets/images/index/img-mainvisual.jpg" alt="Sunset in the mountains">
+									<div class="px-6 py-4">
+										<div class="font-bold text-xl mb-2">Posts Title</div>
+										<p class="text-gray-700 text-base">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+										</p>
+									</div>
+									<div class="px-6 py-4">
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Design</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Animation</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Fature</span>
+									</div>
+								</nuxt-link>
+							</li>
+							<li class="contents-item w-full xl:w-1/4 lg:w-1/3 md:w-1/2 md:p-2 mb-4 md:mb-0">
+								<nuxt-link class="block bg-white rounded-lg overflow-hidden shadow" to="####">
+									<img class="h-56 w-full object-cover object-center" src="/assets/images/index/img-mainvisual.jpg" alt="Sunset in the mountains">
+									<div class="px-6 py-4">
+										<div class="font-bold text-xl mb-2">Posts Title</div>
+										<p class="text-gray-700 text-base">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+										</p>
+									</div>
+									<div class="px-6 py-4">
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Design</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Animation</span>
+										<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2 mr-2">#Fature</span>
+									</div>
+								</nuxt-link>
+							</li>
+						</ul>
+					</div>
+				</section>
+			</article>
 		</div>
 	</div>
 </template>
@@ -27,5 +133,11 @@ export default {
 .mainvisual {
 	background: url(/assets/images/index/img-mainvisual.jpg) no-repeat center center;
 	background-size: cover;
+}
+.contents-item a {
+	transition: all 200ms;
+}
+.contents-item a:hover {
+	transform: scale(1.02);
 }
 </style>
